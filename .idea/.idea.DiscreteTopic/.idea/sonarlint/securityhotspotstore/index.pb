851dd9f8b79c912f9df27108da5a9f219150fd18
Thanh Hoang Xuan <thanh.hoangxuan@alfalaval.com>
2023-10-27 11:27:12 +0200

    init commit

---
+++ .idea/.idea.DiscreteTopic/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,3 @@
+
+P
+ CancellationFramework/Program.cs,0\e\0e8a521553fe27c74270742f3032ea0dd1f3de8f
---
+++ CancellationFramework/Program.cs
@@ -0,0 +1,41 @@
+﻿class Program
+{
+    static async Task Main(string[] args)
+    {
+        CancellationTokenSource cts = new CancellationTokenSource();
+        
+        Console.WriteLine("Press any key to cancel the task...");
+
+        Task longRunningTask = LongRunningOperation(cts.Token);
+
+        // Wait for user input to cancel the task
+        Console.ReadKey();
+        cts.Cancel(); // Signal cancellation
+
+        try
+        {
+            await longRunningTask; // Wait for the task to complete (or be canceled)
+        }
+        catch (OperationCanceledException)
+        {
+            Console.WriteLine("Task was canceled.");
+        }
+    }
+
+    private static async Task LongRunningOperation(CancellationToken ctsToken)
+    {
+        try
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine($"Working ... {i + 1} seconds");
+                await Task.Delay(1000, ctsToken);
+            }
+            Console.WriteLine("Task completed successfully.");
+        }
+        catch (OperationCanceledException ex)
+        {
+            Console.WriteLine("Task was canceled.");
+        }
+    }
+}
